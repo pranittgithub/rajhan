382e3a66f04196725ed24fc0e2fd506f1d24a076
pranittgithub <pranittmankar@gmail.com>
2025-05-05 17:46:15 +0530

    pp plans remaininf

--- assets/js/script.js
+++ assets/js/script.js
@@ -315,3 +315,16 @@
 })(window.jQuery);
 
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
